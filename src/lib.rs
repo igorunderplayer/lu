f32eb4ec07fe48d22e16594a52e967bb8cf63c5b
igorunderplayer <igorschug6@gmail.com>
2022-04-13 16:37:31 -0300

    use buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-pub fn show_matches(compare: &str, text: &str) {
-    for line in text.lines() {
-        if line.contains(compare) {
-            println!("{}", line)
-        }
+use std::{
+    io::{BufReader, BufRead, self},
+    fs::File,
+};
+
+pub fn show_matches(compare: &str, file: BufReader<File>) -> Result<(), io::Error> {
+    for line in file.lines() {
+        match line {
+            Err(e) => println!("Error on line {}", e),
+            Ok(ref v) => {
+                if v.contains(compare) {
+                   println!("{}", line?);
+                }
+            }
+        }    
     }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use std::env;
+use std::{
+    io::BufReader,
+    fs::File,
+    path,
+    env
+};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let match_string = env::args().nth(1).expect("bah");
     let filename = env::args().nth(2).expect("Bah2");
 
-    let path = std::path::PathBuf::from(filename);
-    let content = std::fs::read_to_string(path).expect("Nao foi possivel ler o arquivo");
-    lu::show_matches(&match_string, &content);
+    let path = path::PathBuf::from(filename);
+    let content = BufReader::new(File::open(path).expect("COISO ERRADO"));
+
+    lu::show_matches(&match_string, content)?;
+
+    Ok(())
 }
